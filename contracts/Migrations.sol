c4eec9ad8a8fb5c69a83dfbfdffdb3118893bf7c
TechieTeee <100870737+TechieTeee@users.noreply.github.com>
2022-06-25 16:03:49 -0500

    Update Migrations.sol

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,19 +1,22 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.6.0;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract Migrations {
-  address public owner = msg.sender;
-  uint public last_completed_migration;
+contract BlockFuseMint is ERC721 {
 
-  modifier restricted() {
-    require(
-      msg.sender == owner,
-      "This function is restricted to the contract's owner"
-    );
-    _;
-  }
+  constructor() ERC721("BlockFuseMint", "BFNFT") public  {}
+  event NFTCreated (
 
-  function setCompleted(uint completed) public restricted {
-    last_completed_migration = completed;
+    uint tokenId,
+    string imageURL,
+    uint date,
+    address payable from
+  );
+
+  function mintBlockFuseMintNFT(string memory _tokenURI) external {
+    uint _tokenId = totalSupply().add(1);
+    _safeMint(msg.sender, _tokenId);
+    _setTokenURI(_tokenId, _tokenURI);
+    emit BlockFuseMintNFTCreated(_tokenId, _tokenURI, now, msg.sender);
   }
 }
