96d8742c53a1db317caed73f1195d22b6919b511
webdevbaddie <95775654+webdevbaddie@users.noreply.github.com>
2022-06-25 11:54:55 -0400

    pull

--- src/components/layout/navbar/Navbar.js
+++ src/components/layout/navbar/Navbar.js
@@ -19,7 +19,7 @@ import { StylesProvider } from '@material-ui/core/styles'
 import './Navbar.css'
 import logo from '../../../images/logo.jpg'
 
-export const Navbar = () => {
+export const Navbar = withRouter(({ account, connectWallet }) => {
   const [anchorEl, setAnchorEl] = useState(null)
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState(null)
 
@@ -123,7 +123,36 @@ export const Navbar = () => {
 
             <div className="grow" />
             <div className="sectionDesktop">
-              {/* Add Account  */}
+              {
+                account ? (
+                  <>
+                    <Button className="whiteLink">
+                      {account.substring(0, 8)}...{account.substring(32, 24)}
+                    </Button>
+                    <Button
+                      variant="contained"
+                      className="connected-btn"
+                      endIcon={<VerifiedUserSharpIcon />}
+                    >
+                      Connected
+                    </Button>
+                  </>
+                ) : (
+                  <Button
+                    variant="contained"
+                    className="connect-wallet-btn"
+                    onClick={() => {
+                      connectWallet()
+                    }}
+                  >
+                    Connect Wallet
+                  </Button>
+                )
+
+
+
+
+              }
 
               <IconButton
                 edge="end"
@@ -155,3 +184,4 @@ export const Navbar = () => {
     </StylesProvider>
   )
 }
+)
