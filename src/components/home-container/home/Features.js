c27c809f645723a0da5716b433fec97721f26796
Daphne Charles <dcharles@salesforce.com>
2022-06-26 08:21:03 -0400

    removed icons

--- src/components/home-container/home/Features.js
+++ src/components/home-container/home/Features.js
@@ -1,6 +1,5 @@
 import { ReactElement } from 'react';
 import { Box, SimpleGrid, Icon, Text, Stack, Flex, Heading } from '@chakra-ui/react';
-import { FcAssistant, FcDonate, FcInTransit } from 'react-icons/fc';
 
 const Feature = ({ title, text, icon }) => {
   return (
@@ -30,21 +29,21 @@ export default function SimpleThreeColumns() {
   </Heading>
       <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10}>
         <Feature
-          icon={<Icon as={FcAssistant} w={10} h={10} />}
+          icon={<Icon  w={10} h={10} />}
           title={'For Recruiters'}
           text={
             'Whether you are a web3 company looking to hire full-time or a DAO looking for strong contributors, our platform allows you to quantitatively view candidates based on their talent ranking. Recruiting companies can also partner with our educational organizations to design and curate technical challenges for candidates to complete. As rewards for completing these challenges, candidates earn proof of skill badges which gives them a competitive edge in the recruiting process.'
           }
         />
         <Feature
-          icon={<Icon as={FcDonate} w={10} h={10} />}
+          icon={<Icon  w={10} h={10} />}
           title={'For Candidates'}
           text={
             'Candidates have a platform to consolidate all the aspects of the professional identity scattered in the metaverse (GitHub, NFTs, Twitter, LinkedIn). Candidates have the opportunity to view their talent score based on their proof of skill badges and be provided a path to improving that score. Candidates can participate in sponsored challenges from hiring companies that would put them higher in the recruiting pool.'
           }
         />
         <Feature
-          icon={<Icon as={FcInTransit} w={10} h={10} />}
+          icon={<Icon  w={10} h={10} />}
           title={'For Educators'}
           text={
             'Educational organizations benefit by being provided the infrastructure to aggregate and mint their own proof-of-skill badges on our platform. In addition, they are rewarded when candidates who hold their proof of skill badges are hired. Educators strengthen the reputation of their curriculum by being ranked higher with the BlockFuse algorithm, thereby enticing more learners to take those courses.'
