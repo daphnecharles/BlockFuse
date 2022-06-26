0706f5470927832e84fc0851e831074caa6b6fd1
TechieTeee <100870737+TechieTeee@users.noreply.github.com>
2022-06-26 05:40:34 -0500

    Update and rename BlockFuseToken.sol to BlockFuseTokenon.sol

--- contracts/BlockFuseTokenon.sol
+++ contracts/BlockFuseTokenon.sol
@@ -5,7 +5,7 @@ import "./ConvertLib.sol";
 // coin/token contracts. If you want to create a standards-compliant
 // token, see: https://github.com/ConsenSys/Tokens. Cheers!
 
-contract BlockFuse Token {
+contract BlockFuseToken {
 	mapping (address => uint) balances;
 
 	event Transfer(address indexed _from, address indexed _to, uint256 _value);
